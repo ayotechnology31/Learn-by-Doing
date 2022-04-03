7d9e184a0ffcc2bbf01439a75c2af0d504e4001e
Amphavanh Lithyouvong <alithyouvong@gmail.com>
2022-04-03 18:01:42 -0500

    Initial Commit

---
+++ Shared/CardModel.swift
@@ -0,0 +1,21 @@
+//
+//  CardModel.swift
+//  Learn by Doing
+//
+//  Created by Amphavanh Lithyouvong on 2/22/22.
+//
+
+import SwiftUI
+
+// MARK: - CARD MODEL
+
+struct Card: Identifiable {
+    var id = UUID()
+    var title: String
+    var headline: String
+    var imageName: String
+    var callToAction: String
+    var message: String
+    var gradientColors: [Color]
+    
+}
---
+++ Shared/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Shared
+//
+//  Created by Amphavanh Lithyouvong on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
